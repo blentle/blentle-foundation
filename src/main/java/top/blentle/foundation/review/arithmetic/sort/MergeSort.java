92ea79422f12938161c143bd36f0c06252d13fb7
blentle <blentle@sina.com>
2017-03-20 11:19:10 +0800

    test merge sort arithmetic, add annotation  but not finished !

--- src/main/java/top/blentle/foundation/review/arithmetic/sort/MergeSort.java
+++ src/main/java/top/blentle/foundation/review/arithmetic/sort/MergeSort.java
@@ -10,14 +10,14 @@ import top.blentle.foundation.review.arithmetic.Data;
  * @description: merge sort
  * @since: 1.0
  */
-public class MergeSort extends Data{
+public class MergeSort extends Data {
 
     private static Integer[] data = getData().toArray(new Integer[getData().size()]);
 
     public static void sort() {
         //todo : divide data into two segments ...
         System.err.println("sort after:");
-        for(int number : data) {
+        for (int number : data) {
             System.out.print(" " + number + " ");
         }
     }
