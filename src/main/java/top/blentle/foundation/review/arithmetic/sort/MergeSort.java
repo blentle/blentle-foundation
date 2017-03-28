0a2c98c6ddc69c3d64ad76b0391d9fc8acbf81ce
blentle <blentle@sina.com>
2017-03-28 15:31:51 +0800

    finished merge sort implementation

--- src/main/java/top/blentle/foundation/review/arithmetic/sort/MergeSort.java
+++ src/main/java/top/blentle/foundation/review/arithmetic/sort/MergeSort.java
@@ -14,15 +14,42 @@ public class MergeSort extends Data {
 
     private static Integer[] data = getData().toArray(new Integer[getData().size()]);
 
-    public static void sort() {
-        //todo : divide data into two segments ...
-        System.err.println("sort after:");
-        for (int number : data) {
-            System.out.print(" " + number + " ");
+    public static void sort(Integer[] data, int leftIndex, int rightIndex) {
+        if (leftIndex >= rightIndex)
+            return;
+        int midIndex = (leftIndex + leftIndex) >>> 1;
+        sort(data,leftIndex ,midIndex);
+        sort(data,midIndex + 1, rightIndex);
+        merge(data,leftIndex,midIndex,rightIndex);
+    }
+
+    private static void merge(Integer[] data, int leftIndex, int midIndex, int rightIndex) {
+        int[] tmp = new int[data.length];
+        int r1 = midIndex + 1;
+        int left = leftIndex;
+        int cIndex = leftIndex;
+        while(leftIndex <= midIndex && r1 <= rightIndex) {
+            if(data[leftIndex] <= data[r1])
+                tmp[left ++] = data[leftIndex ++];
+            else
+                tmp[left ++] = data[r1 ++];
+        }
+        while(leftIndex <= midIndex)
+            tmp[left ++] = data[leftIndex ++];
+        while(r1 <= rightIndex)
+            tmp[left ++] = data[r1 ++];
+        while(cIndex <= rightIndex) {
+            data[cIndex]=tmp[cIndex];
+            cIndex ++;
         }
     }
 
+
     public static void main(String[] args) {
-        sort();
+        sort(data, 0, data.length - 1);
+        System.err.println("sort after:");
+        for(int number : data) {
+            System.out.print(" " + number + " ");
+        }
     }
 }
