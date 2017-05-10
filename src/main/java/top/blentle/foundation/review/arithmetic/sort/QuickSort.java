1b938d5979289e3afffb84581ffa99a9b3bab146
blentle <blentle@sina.com>
2017-05-10 12:58:35 +0800

    implementations

---
+++ src/main/java/top/blentle/foundation/review/arithmetic/sort/QuickSort.java
@@ -0,0 +1,53 @@
+package top.blentle.foundation.review.arithmetic.sort;
+
+import top.blentle.foundation.review.arithmetic.Data;
+
+/**
+ * @author: blentle
+ * @group: rd
+ * @createdate: 2017/3/18 14:41
+ * @mail: blentle.huan.ren@gmail.com
+ * @description: quick  sort
+ * @since: 1.0
+ */
+public class QuickSort {
+    private static Integer[] data = {12, 45, 458, 90, 87, 5789, 23, 45, 789, 13, 897, 33456, 405, 768, 907, 876, 100, 78, 333, 597, 980};
+
+    public static void quickSort(Integer[] data, int l, int r) {
+        int i = l;
+        int j = r;
+        int pvoit = data[(l + r) / 2];
+        while (i <= j) {
+            while (data[i] < pvoit) {
+                i++;
+            }
+            while (data[j] > pvoit) {
+                j--;
+            }
+            if (i <= j) {
+                int temp = data[i];
+                data[i] = data[j];
+                data[j] = temp;
+                i++;
+                j--;
+            }
+        }
+        if (l < j)
+            quickSort(data, l, j);
+        if (i < r)
+            quickSort(data, i, r);
+
+    }
+
+    public static void main(String[] args) {
+        for (int number : data) {
+            System.out.print(" " + number);
+        }
+        System.out.print("\r\n");
+        quickSort(data, 0, data.length - 1);
+        for (int number : data) {
+            System.out.print(" " + number);
+        }
+        System.out.print("\r\n");
+    }
+}
