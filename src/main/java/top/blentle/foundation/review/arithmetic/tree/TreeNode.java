48a1d376e8851cffa96445ccc7bfd948d7a6663b
renhuan <renhuan>
2017-12-29 17:55:29 +0800

    quickSort review

--- src/main/java/top/blentle/foundation/review/arithmetic/tree/TreeNode.java
+++ src/main/java/top/blentle/foundation/review/arithmetic/tree/TreeNode.java
@@ -4,10 +4,10 @@ package top.blentle.foundation.review.arithmetic.tree;
  * @author :  renhuan
  * @email : blentle.huan.ren@gmail.com
  * @time :  2017/12/29 0029
- * @description :
+ * @description : 二叉树的一个树节点
  * @since : 1.0
  */
-public class TreeNode<T> {
+public class TreeNode<T extends Comparable<? super T>> {
 
     /**
      * 节点上的数据
@@ -28,6 +28,25 @@ public class TreeNode<T> {
         this.data = data;
         this.left = left;
         this.right = right;
+    }
+
+    /**
+     * @param t    插入的数据
+     * @param root 原树的根节点
+     * @return 插入后的根节点
+     */
+    public TreeNode<T> insert(T t, TreeNode<T> root) {
+        //返回新的二叉树的跟节点
+        if (root == null) {
+            return new TreeNode<T>(t, null, null);
+        }
+        int result = t.compareTo(root.data);
+        if(result  < 0)
+            root.left = insert(t,root.left);
+        else if(result > 0)
+            root.right = insert(t,root.right);
+        return root;
+
     }
 
 
