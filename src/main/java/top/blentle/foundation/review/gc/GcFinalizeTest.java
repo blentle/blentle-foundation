f738935ef0fb80a36efa9f03fbd77758ff38a9fb
blentle <blentle@sina.com>
2018-01-07 11:21:59 +0800

    测试垃圾收集

--- src/main/java/top/blentle/foundation/review/gc/GcFinalizeTest.java
+++ src/main/java/top/blentle/foundation/review/gc/GcFinalizeTest.java
@@ -6,8 +6,18 @@ import java.io.Serializable;
  * Created by blentle on 2017/2/13.
  */
 public class GcFinalizeTest implements Serializable {
+
+    private static final int mem = 1024 * 1024;
+
+    public static void testAllocation() {
+        byte[] allocation1, allocation2, allocation3, allocation4;
+        allocation1 = new byte[2 * mem];
+        allocation2 = new byte[2 * mem];
+        allocation3 = new byte[2 * mem];
+        allocation1 = new byte[4 * mem];
+    }
+
     public static void main(String[] args) {
-        byte a = (byte)257;
-        System.err.println(a);
+        testAllocation();
     }
 }
