4e742192c65b1fa3003b152777a09006daaa80d8
renhuan <renhuan>
2018-02-06 17:53:43 +0800

    remove unused avl node and add red black tree !

--- src/main/java/top/blentle/foundation/review/arithmetic/tree/rb/RBTree.java
+++ src/main/java/top/blentle/foundation/review/arithmetic/tree/rb/RBTree.java
@@ -65,6 +65,8 @@ public class RBTree {
             root = new TreeNode(value);
             //根节点是黑色的
             root.red = false;
+            //创建根节点后，直接返回
+            return;
         }
         TreeNode parent = null;
         while (t != null) {
@@ -80,7 +82,7 @@ public class RBTree {
         t = new TreeNode(value);
         //parent是上一次循环后的t
         t.parent = parent;
-        if(parent != null) {
+        if (parent != null) {
             if (t.val < parent.val)
                 parent.left = t;
             else
@@ -94,21 +96,77 @@ public class RBTree {
 
     /**
      * 插入后新插入的节点进行从下向上变色，和旋转处理
+     *
      * @param t
+     * @solutions :
+     * 1. 插在左子树上，且父节点是祖父的左子节点：
+     * 父节点变黑，祖父节点变红， 右旋祖父节点
+     * 2. 插在左子树上，且父节点是祖父的右子节点：
+     * 右旋父节点，并把插入节点指向父节点，即原来的父节点变成了插入节点（新节点），
+     * 新节点的父节点变黑 ，新节点的祖父节点变红， 左旋新节点的祖父节点 【这里参考对象变了，所以描述的比较啰嗦】
+     * 3. 插在右子树上，且父节点是祖父的右子节点：
+     * 父节点变黑，祖父节点变红，左旋祖父节点  (跟 2 的第二步一模一样)
+     * 4. 插在右子树上，且父节点是祖父的左子节点：
+     * 左旋父节点，并把插入节点指向父节点，即原来的父节点变成的插入节点（新节点）,
+     * 新节点的父节点变黑 ，新节点的祖父节点变红， 右旋新节点的祖父节点 【这里参考对象变了，所以描述的比较啰嗦】
      */
     private void fixAfterInsertion(TreeNode t) {
-        TreeNode parent = t.parent;
-        TreeNode uncle = uncleOf(t);
-        //父亲是红色
-        if(parent.red) {
+        while (t != null && t != root && t.red && t.parent.red) {
+            TreeNode parent = t.parent;
+            TreeNode grand = parentOf(parent);
+            TreeNode uncle = uncleOf(t);
             //叔叔是null或者是黑色, 即叔叔都是黑色
-            if(uncle == null || (!uncle.red)) {
-                //todo:
+            if (uncle == null || (!uncle.red)) {
+                //1.插在左子树上
+                if (parent.left == t) {
+                    //1.1. 父节点是祖父的左子节点:  父节点变黑，祖父节点变红， 右旋祖父节点
+                    if (parent == grand.left) {
+                        setColor(parent, false);
+                        setColor(grand, true);
+                        rotateRight(grand);
+                    } else {
+                        //1.2. 父节点是祖父的右子节点:
+                        // 右旋父节点，并把插入节点指向父节点，即原来的父节点变成了插入节点（新节点），下面这一步就递归了。
+                        // 新节点的父节点变黑 ，新节点的祖父节点变红， 左旋新节点的祖父节点
+                        t = parent;
+                        rotateRight(parent);
+                    }
+                } else {
+                    //2. 插在右子树上
+                    //2.1. 父节点是祖父的右子节点:  父节点变黑，祖父节点变红，左旋祖父节点
+                    if (parent == grand.right) {
+                        setColor(parent, false);
+                        setColor(grand, true);
+                        rotateLeft(grand);
+                    } else {
+                        //2.2. 父节点是祖父的左子节点:
+                        //左旋父节点，并把插入节点指向父节点，即原来的父节点变成的插入节点（新节点）,
+                        //新节点的父节点变黑 ，新节点的祖父节点变红， 右旋新节点的祖父节点
+                        t = parent;
+                        rotateLeft(parent);
+
+                    }
+                }
             } else {
                 //叔叔是红色,直接把叔叔和父亲变成黑色，爷爷变成红色
-                //todo:
+                //父亲,叔叔变黑
+                setColor(parentOf(t), false);
+                setColor(uncleOf(t), false);
+                //爷爷变红
+                setColor(grandfatherOf(t), true);
             }
         }
+    }
+
+    /**
+     * 给指定的节点上色
+     *
+     * @param treeNode
+     * @param b        false : 黑色，true : 红色
+     */
+    private void setColor(TreeNode treeNode, boolean b) {
+        if (treeNode != null)
+            treeNode.red = b;
     }
 
     /**
@@ -125,7 +183,7 @@ public class RBTree {
         right.parent = node.parent;
         //重新给父节点的孩子赋值
         if (node.parent == null)
-            root = node;
+            root = right;
         else if (node.parent.left == node)
             node.parent.left = right;
         else
@@ -149,7 +207,7 @@ public class RBTree {
         left.parent = node.parent;
         //重新给父节点的孩子赋值
         if (node.parent == null)
-            root = node;
+            root = left;
         else if (node.parent.left == node)
             node.parent.left = left;
         else
@@ -171,13 +229,14 @@ public class RBTree {
 
     /**
      * 获取指定节点node的叔叔节点(父亲的兄弟)
+     *
      * @param node
      * @return
      */
     private TreeNode uncleOf(TreeNode node) {
         TreeNode parent = parentOf(node);
         TreeNode left = leftOf(grandfatherOf(node));
-        if(parent == left)
+        if (parent == left)
             return rightOf(grandfatherOf(node));
         else
             return left;
@@ -214,6 +273,10 @@ public class RBTree {
     }
 
 
+    /**
+     * 断点测试一下
+     * @param args
+     */
     public static void main(String[] args) {
         RBTree obj = new RBTree();
         obj.add(4);
@@ -221,6 +284,9 @@ public class RBTree {
         obj.add(1);
         obj.add(5);
         obj.add(6);
+        obj.add(10);
+        obj.add(8);
         System.err.println(obj.root);
     }
+    //嗯，画出图来，好使
 }
