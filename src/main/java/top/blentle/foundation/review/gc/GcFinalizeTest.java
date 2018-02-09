cc67257861a215d92b3c4f732a37ef0e4ce7846c
renhuan <renhuan>
2018-02-09 10:39:37 +0800

    remove unused avl node and add red black tree !

--- src/main/java/top/blentle/foundation/review/gc/GcFinalizeTest.java
+++ src/main/java/top/blentle/foundation/review/gc/GcFinalizeTest.java
@@ -6,5 +6,46 @@ import java.io.Serializable;
  * Created by blentle on 2017/2/13.
  */
 public class GcFinalizeTest implements Serializable {
+//    /**
+//     * n是自然数
+//     * 递归实现
+//     * @param n
+//     * @return
+//     */
+//    public static int fibonacci(int n) {
+//        if (n == 0)
+//            return 0;
+//        if (n == 1 || n == 2)
+//            return 1;
+//        return fibonacci(n - 2) + fibonacci(n - 1);
+//    }
 
+
+    /**
+     * n是自然数
+     * 非递归实现
+     *
+     * @param n
+     * @return
+     */
+    public static int fibonacci(int n) {
+        if (n == 0)
+            return 0;
+        if (n == 1 || n == 2)
+            return 1;
+        int a, b;
+        int sum = 0;
+        a = b = 1;
+        //这里从2开始遍历
+        for (int i = 2; i < n; i++) {
+            sum = a + b;
+            a = b;
+            b = sum;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.err.println(fibonacci(50));
+    }
 }
